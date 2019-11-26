f8ff78eeb74e79b3133e6b83f6816257a9cdb9a5
profmou <whmou@nudt.edu.cn>
2019-11-27 01:11:21 +0800

    1、增加点击按键，进行连接网络测试的代码

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -30,6 +30,12 @@ import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.model.LatLng;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+//import java.net.InetAddress;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -192,6 +198,34 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
     }
 
+    //点击按键网络连接相关代码
+    public void onClick(View v) {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    acceptServer();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private void acceptServer() throws IOException {
+        //1.创建客户端Socket，指定服务器地址和端口
+        Socket socket = new Socket("106.13.21.39", 9999);
+        //2.获取输出流，向服务器端发送信息
+        OutputStream os = socket.getOutputStream();//字节输出流
+        PrintWriter pw = new PrintWriter(os);//将输出流包装为打印流
+        //获取客户端的IP地址
+        //InetAddress address = InetAddress.getLocalHost();
+        //String ip = address.getHostAddress();
+        pw.write("客户端：~" + "测试网络" + "~ 接入服务器！！");
+        pw.flush();
+        socket.shutdownOutput();//关闭输出流
+        socket.close();
+    }
 
      /*   public void sendMessage(View view) {
                     //Do something in response to button
